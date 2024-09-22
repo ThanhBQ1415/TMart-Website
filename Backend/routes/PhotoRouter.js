c15ecdc05f2d48d1f23082424918e4ac4a9db941
ThanhBQ1415 <buithanh29117@gmai.com>
2024-09-22 19:05:31 +0700

    update

--- Backend/routes/PhotoRouter.js
+++ Backend/routes/PhotoRouter.js
@@ -11,55 +11,9 @@ const auth = require("../middleware/auth.middleware")
 
 
 
-// [post] /api/photo/commentsOfPhoto/:photo_id
-router.post(
-  "/commentsOfPhoto/:photo_id", 
-  (req, res, next) => auth.hasSessionRecord(req,res,next),
-  async (request, response) => {
-    console.log(request.body)
-    const photo = await Photo.findOne({_id: request.body.photo_id})
 
-    if(photo){
-      const Comment = {
-        comment: request.body.cmt, 
-        date_time: new Date().toISOString(),
-        user_id: request.cookies.user_id
-      };
-      if (!photo.comments) photo.comments = [Comment];
-      else{
-        photo.comments.push(Comment);    
-      }
-      photo.save();
-      console.log("** Server: a new comment added! **");
-      response.status(200).send(); 
-    }
-    else{
-      response.status(400).json({ message: "Server: Photo you just commented is not found" });
-    }
-  }
-)
 
 // [get] /api/photo/photosOfUser/:id 
-router.get(
-  "/photosOfUser/:id", 
-  async (request, response) => {
-    try {
-      const id = request.params.id;
-      const photosData = await Photo.find({ user_id: id });
-      const photos = JSON.parse(JSON.stringify(photosData))
-      for(const photo of photos) {
-        if(photo.comments.length > 0){
-          for (const cmt of photo.comments) {
-            const user = await User.findOne({_id: cmt.user_id})
-            cmt.user = user
-          }
-          
-        }
-      }
-      response.send(photos);
-    } catch (error) {
-      response.status(500).send({ error });
-    }
-});
+
 
 module.exports = router;
