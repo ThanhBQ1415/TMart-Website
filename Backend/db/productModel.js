229fcb9bad3f5f8b242aba3f3fe7f8662befffef
ThanhBQ1415 <timvechanque7@gmail.com>
2024-09-22 19:28:36 +0700

    update

--- Backend/db/productModel.js
+++ Backend/db/productModel.js
@@ -6,6 +6,7 @@ const productSchema = new mongoose.Schema({
   file_name: { type: String },
   description: { type: String, default: "" },
   type_product: {type: String},
+  name: { type: String, default: "" },
 });
 
 module.exports = mongoose.model.Product || mongoose.model("Product", productSchema);
